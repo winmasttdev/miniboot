c38f3b4efc12fb2a23dcd84275f4f95ee52bc458
winmastt <winmastt@localhost>
2026-09-19 14:02:52 +0300

    Pages site content

--- scripts/miniboot-refresh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Refresh MiniBoot ESP files (run after kernel updates).
-set -e
-mkinitcpio -c /etc/mkinitcpio.d/miniboot-switch.conf -k /boot/vmlinuz-linux -g /boot/miniboot-switch.img
-# NOTE: miniboot boots its OWN kernel (must match its /usr/lib/modules).
-cp /opt/miniboot-root/boot/vmlinuz-linux /boot/efi/EFI/miniboot/vmlinuz-linux
-cp /boot/miniboot-switch.img /boot/efi/EFI/miniboot/miniboot-switch.img
